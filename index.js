dc0c5ba8a462b11082b9fa330a4ec2ccd1858594
Gillian-C-Gonzales <gillian.gonzales22@gmail.com>
2025-11-01 02:13:30 -0400

    Added simple calculation buttons - Subtract - Multiply - Divide

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 plus = document.getElementById('plus');
+minus = document.getElementById('minus');
+multiply = document.getElementById('multiply');
+divide = document.getElementById('divide');
 
 plus.addEventListener('click', () => {
     const added = parseInt(document.getElementById("value1").value) + parseInt(document.getElementById("value2").value);
     document.getElementById("result").innerHTML = added;
 });
+
+minus.addEventListener('click', () => {
+    const subbed = parseInt(document.getElementById("value1").value) - parseInt(document.getElementById("value2").value);
+    document.getElementById("result").innerHTML = subbed;
+});
+
+multiply.addEventListener('click', () => {
+    const timesed = parseInt(document.getElementById("value1").value) * parseInt(document.getElementById("value2").value);
+    document.getElementById("result").innerHTML = timesed;
+});
+
+
+divide.addEventListener('click', () => {
+    const slashed = parseFloat(document.getElementById("value1").value) / parseFloat(document.getElementById("value2").value);
+    document.getElementById("result").innerHTML = slashed;
+});
+
+
